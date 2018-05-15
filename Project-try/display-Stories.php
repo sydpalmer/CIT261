a8ce568a63a68ff65a05e36df22a2153e2b0988d
sydpalmer <hay14004@byui.edu>
2018-05-15 11:00:47 -0600

    fixed problems

--- Project-try/display-Stories.php
+++ Project-try/display-Stories.php
@@ -3,51 +3,22 @@
 <body>
 <table>
 <?php
-  // $host = 'host=localhost';
-  // $port = 'port=5432';
-  // $dbName = 'dbname=StoriesDB';
-  // $user = 'user=postgres';
-  // $password = 'password=SydGrad2014';
-  // $db = pg_connect("$host $port $dbName $user $password");
+  $link = pg_connect("dbname=StoriesDB user=postgres password=SydGrad2014");
+  $res = pg_exec($link, "select * from stories");
+  $numrows = pg_numrows($res);
 
-  // if(!$db) {
-  //   echo "Error : Unable to open database\n";
-  // } else {
-  //   echo "Opened database successfully\n";
-  // }
-
-  // $sql = "SELECT * FROM stories";
-
-  // $result = pg_query($db, $sql);
-  // if(!$result){
-  //   echo pg_last_error($db);
-  //   exit;
-  // }
-
-  // $count = 1;
-  // while($row = pg_fetch_all($result)){
-  //   echo "<tr style='text-align: center;' id='" . $count . "'>";
-  //   echo "<td>" . $row[1] . "</td>";
-  //   echo "<td>" . $row[2] . "</td>";
-  //   echo "<td>" . $row[3] . "</td>";
-  //   echo "</tr>";
-  //   $count += 1;
-  // }
-  // pg_close($db);
-
-    $link = pg_connect("dbname=StoriesDB user=postgres password=SydGrad2014");
-    $res = pg_exec($link, "select * from stories");
-    $numrows = pg_numrows($res);
-
-    for($ri = 0; $ri < $numrows; $ri++){
-      echo "<tr style='text-align: center;' id='" . $ri . "'>";
-      $row = pg_fetch_array($res, $ri);
-      echo "<td>" . $row['title'] . "</td>";
-      echo "<td>" . $row['author'] . "</td>";
-      echo "<td>" . $row['story'] . "</td>";
-      echo "</tr>";
-    }
-    pg_close($link);
+  echo "<tr style='text-align: center'><th>Title</th><th>Author</th><th>Story</th></tr>";
+  $count = 0;
+  for($ri = 0; $ri < $numrows; $ri++){
+    $count = $ri + 1;
+    echo "<tr style='text-align: right;' id='" . $count . "'>";
+    $row = pg_fetch_array($res, $ri);
+    echo "<td>" . $row['title'] . "</td>";
+    echo "<td>" . $row['author'] . "</td>";
+    echo "<td>" . $row['story'] . "</td>";
+    echo "</tr>";
+  }
+  pg_close($link);
 
 ?> 
 </table>
