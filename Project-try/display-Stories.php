7e3ebf80ab775e5af322d395ee6e31a8cf768ade
sydpalmer <hay14004@byui.edu>
2018-05-15 10:35:05 -0600

    try

--- Project-try/display-Stories.php
+++ Project-try/display-Stories.php
@@ -3,51 +3,51 @@
 <body>
 <table>
 <?php
-  $host = 'host=localhost';
-  $port = 'port=5432';
-  $dbName = 'dbname=StoriesDB';
-  $user = 'user=postgres';
-  $password = 'password=SydGrad2014';
-  $db = pg_connect("$host $port $dbName $user $password");
+  // $host = 'host=localhost';
+  // $port = 'port=5432';
+  // $dbName = 'dbname=StoriesDB';
+  // $user = 'user=postgres';
+  // $password = 'password=SydGrad2014';
+  // $db = pg_connect("$host $port $dbName $user $password");
 
-  if(!$db) {
-    echo "Error : Unable to open database\n";
-  } else {
-    echo "Opened database successfully\n";
-  }
+  // if(!$db) {
+  //   echo "Error : Unable to open database\n";
+  // } else {
+  //   echo "Opened database successfully\n";
+  // }
 
-  $sql = "SELECT * FROM stories";
+  // $sql = "SELECT * FROM stories";
 
-  $result = pg_query($db, $sql);
-  if(!$result){
-    echo pg_last_error($db);
-    exit;
-  }
+  // $result = pg_query($db, $sql);
+  // if(!$result){
+  //   echo pg_last_error($db);
+  //   exit;
+  // }
 
-  $count = 1;
-  while($row = pg_fetch_all($result)){
-    echo "<tr style='text-align: center;' id='" . $count . "'>";
-    echo "<td>" . $row[1] . "</td>";
-    echo "<td>" . $row[2] . "</td>";
-    echo "<td>" . $row[3] . "</td>";
-    echo "</tr>";
-    $count += 1;
-  }
-  pg_close($db);
+  // $count = 1;
+  // while($row = pg_fetch_all($result)){
+  //   echo "<tr style='text-align: center;' id='" . $count . "'>";
+  //   echo "<td>" . $row[1] . "</td>";
+  //   echo "<td>" . $row[2] . "</td>";
+  //   echo "<td>" . $row[3] . "</td>";
+  //   echo "</tr>";
+  //   $count += 1;
+  // }
+  // pg_close($db);
 
-    // $link = pg_connect("dbname=StoriesDB user=postgres password=SydGrad2014");
-    // $res = pg_exec($link, "select * from stories");
-    // $numrows = pg_numrows($res);
+    $link = pg_connect("dbname=StoriesDB user=postgres password=SydGrad2014");
+    $res = pg_exec($link, "select * from stories");
+    $numrows = pg_numrows($res);
 
-    // for($ri = 0; $ri < $numrows; $ri++){
-    //   echo "<tr style='text-align: center;' id='" . $ri . "'>";
-    //   $row = pg_fetch_array($res, $ri);
-    //   echo "<td>" . $row['title'] . "</td>";
-    //   echo "<td>" . $row['author'] . "</td>";
-    //   echo "<td>" . $row['story'] . "</td>";
-    //   echo "</tr>";
-    // }
-    // pg_close($link);
+    for($ri = 0; $ri < $numrows; $ri++){
+      echo "<tr style='text-align: center;' id='" . $ri . "'>";
+      $row = pg_fetch_array($res, $ri);
+      echo "<td>" . $row['title'] . "</td>";
+      echo "<td>" . $row['author'] . "</td>";
+      echo "<td>" . $row['story'] . "</td>";
+      echo "</tr>";
+    }
+    pg_close($link);
 
 ?> 
 </table>
