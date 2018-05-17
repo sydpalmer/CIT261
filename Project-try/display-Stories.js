7648e8b546cd861c67b44aba2d31763c54a25601
sydpalmer <hay14004@byui.edu>
2018-05-17 17:53:24 -0600

    additions

--- Project-try/display-Stories.js
+++ Project-try/display-Stories.js
@@ -1,6 +1,7 @@
 document.addEventListener('click', showStory);
 function showStory(){
     if(!isNaN(parseInt(event.srcElement.id))){
+        console.log("Id: " + event.srcElement.id);
         var dest = "showStory.php?id=" + event.srcElement.id;
         var x = (screen.width/2) - (300/2);
         var y = (screen.height/2) - (400/2);
--- Project-try/display-Stories.php
+++ Project-try/display-Stories.php
@@ -2,22 +2,29 @@
 <html>
 <head>
   <script src="display-Stories.js"></script>
+  <style>
+    td{
+      background-color: lightgrey;
+      border: 3px solid black;
+      padding: 10px;
+      font-size: 110%;
+    }
+  </style>
 </head>
 <body>
-<table>
+<table width=100%>
 <?php
   $link = pg_connect("dbname=StoriesDB user=postgres password=SydGrad2014");
   $res = pg_exec($link, "select * from stories");
   $numrows = pg_numrows($res);
 
-  echo "<tr style='text-align: center'><th>Title</th><th>Author</th></tr>";
   $count = 0;
   for($ri = 0; $ri < $numrows; $ri++){
     $count = $ri + 1;
-    echo "<tr style='text-align: right;'>";
+    echo "<tr style='text-align: left;'>";
     $row = pg_fetch_array($res, $ri);
-    echo "<td id='" . $count . "'>" . $row['title'] . "</td>";
-    echo "<td id='" . $count . "'>" . $row['author'] . "</td>";
+    echo "<td id='" . $count . "'><b>" . $row['title'] . "</b><br>";
+    echo "Created by: " . $row['author'] . "</td>";
     echo "</tr>";
   }
   pg_close($link);
