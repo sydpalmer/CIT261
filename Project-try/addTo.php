ce7375b543ad9a29d0c454b94391eba46929ada6
sydpalmer <hay14004@byui.edu>
2018-05-24 10:07:26 -0600

    making it fit well

--- Project-try/addTo.php
+++ Project-try/addTo.php
@@ -12,7 +12,7 @@
     <h3>Adding to <i><?php session_start(); echo $_SESSION['title'];?></i><br>
         Created By: <?php session_start(); echo $_SESSION['author'];?>
     </h3>
-    <div style="width:90%; height:70%;">
+    <div style="width:95%; height:60vh;">
         <textarea style="width:100%; height:100%"></textarea>
     </div>
 </body>
