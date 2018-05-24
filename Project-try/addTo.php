6180b49e0027fd880f8716e2d443d4e1de39bd24
sydpalmer <hay14004@byui.edu>
2018-05-24 10:00:37 -0600

    updating addTo

--- Project-try/addTo.php
+++ Project-try/addTo.php
@@ -2,11 +2,18 @@
 <html>
 <head>
     <title>Add to the Story</title>
+    <style>
+        textarea{
+            resize: none;
+        }
+    </style>
 </head>
 <body>
     <h3>Adding to <i><?php session_start(); echo $_SESSION['title'];?></i><br>
         Created By: <?php session_start(); echo $_SESSION['author'];?>
     </h3>
-    <textarea></textarea>
+    <div style="width:90%; height:70%;">
+        <textarea style="width:100%; height:100%"></textarea>
+    </div>
 </body>
 </html>
