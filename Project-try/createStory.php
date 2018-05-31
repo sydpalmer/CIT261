91d8e10ba9e0f784ccb421b75aba9bdf581c04e8
sydpalmer <hay14004@byui.edu>
2018-05-31 10:22:23 -0600

    styles

--- Project-try/createStory.php
+++ Project-try/createStory.php
@@ -6,6 +6,15 @@
         textarea{
             resize: none;
         }
+        button{
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+            border-radius: 4px;
+            background-color: white;
+            padding: 10px 10px;
+            text-align: center;
+            cursor: pointer;
+            font-size: 95%;
+        }
     </style>
 </head>
 <body style="background-color: cornflowerblue;">
