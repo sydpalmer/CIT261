f3c9f7849ace78887279c08069d185f6c71b99e2
sydpalmer <hay14004@byui.edu>
2018-06-18 17:36:37 -0600

    quiz ninja update

--- Ninja-JS examples/Quiz Ninja/main.js
+++ Ninja-JS examples/Quiz Ninja/main.js
@@ -1,4 +1,13 @@
 alert('Welcome to Quiz Ninja!');
+
+const url = 'http://spbooks.github.io/questions.json';
+fetch(url)
+.then(res => res.json())
+.then(quiz => {
+    view.start.addEventListener('click', () => game.start(quiz.questions), false);
+    view.response.addEventListener('click', (event) => game.check(event), false);
+});
+
 const quiz = [
     { name: "Superman",realName: "Clark Kent" },
     { name: "Wonder Woman",realName: "Diana Prince" },
