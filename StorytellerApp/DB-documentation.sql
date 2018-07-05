a3d1428285c2f871e4ff306557eefb8fa8dfb6a6
sydpalmer <hay14004@byui.edu>
2018-07-05 09:58:59 -0600

    updating database

--- StorytellerApp/DB-documentation.sql
+++ StorytellerApp/DB-documentation.sql
@@ -1,8 +1,15 @@
 --database: StoriesDB
 
+CREATE TABLE user(
+    user_id SERIAL PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
+
 CREATE TABLE stories(
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     author TEXT NOT NULL,
-    story TEXT NOT NULL
+    story TEXT NOT NULL,
+    user_id INTEGER REFERENCES user(user_id)
 );
